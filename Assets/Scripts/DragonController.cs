89ff7ebc5c80b3d2256c55abc453316bc5d392ac
Murilo <theodoro.murilo@hotmail.com>
2020-09-23 16:56:40 -0300

    Core Gameplay
    Core Gameplay is done. Local multiplayer, supports keyboard+mouse and gamepads, has shield and a basic spell.
    
    Next steps: 
    - Include UI for players, shield and egg
    - Create splash, victory and defeat screens
    - Design a basic arena
    - Add more spells
    - Add more arenas
    
    Known Bugs:
    - Sometimes the egg goes through walls

---
+++ Assets/Scripts/DragonController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonController : MonoBehaviour
+{
+    #region REFERENCES
+    public Rigidbody2D rb;
+    #endregion
+    
+    #region PARAMETERS
+    public float moveSpeed;
+    public int maxHealth;
+    
+    public Vector2 startDirection = Vector2.right;
+    public float speedControllerTime = 0.5f;
+
+    public float minimalTimeToChaos;
+    public float chanceToChaos;
+    public float chaosParameter;
+    #endregion
+
+    #region ATTRIBUTES
+    Vector2 lastVelocity;
+    int curHealth;
+
+    private float framesToChaos;
+    #endregion
+
+    #region METHODS
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    bool Chaos()
+    {
+        if (Random.Range(0f, 1f) > chanceToChaos)
+            return false;
+
+        Vector2 perp = Vector2.Perpendicular(rb.velocity) * Random.Range(-chaosParameter, chaosParameter); ;
+
+        Vector2 newDirection = (rb.velocity + perp).normalized;
+
+        ChangeVelocity(newDirection);
+        return true;
+    }
+
+    void ChangeVelocity(Vector2 direction)
+    {
+        rb.velocity = direction.normalized * moveSpeed;
+        if (direction.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        if (curHealth - dmg <= 0)
+            Death();
+        else
+            curHealth -= dmg;
+    }
+
+    IEnumerator SpeedController()
+    {
+        for (;;)
+        {
+            rb.velocity = rb.velocity.normalized * moveSpeed;
+            yield return new WaitForSeconds(speedControllerTime);
+        }
+    }
+    #endregion
+
+    #region UNITY
+    void Start()
+    {
+        ChangeVelocity(startDirection);
+
+        framesToChaos = minimalTimeToChaos * 60;
+
+        StartCoroutine(SpeedController());
+
+        curHealth = maxHealth;
+    }
+    
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+
+        framesToChaos += Time.deltaTime;
+        if (framesToChaos >= minimalTimeToChaos)
+        {
+            if(Chaos())
+                framesToChaos = 0;
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            Vector2 direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+            ChangeVelocity(direction);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerJoinBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerJoinBehaviour : MonoBehaviour
+{
+    #region REFERENCES
+    #endregion
+
+    #region PARAMETERS
+    public List<Vector3> playerSpawnPositions;
+
+    public List<Vector2> playerSpawnRotations;
+    #endregion
+
+    #region EVENTS
+    public void OnPlayerJoin(PlayerInput playerInput)
+    {
+        playerInput.transform.position = playerSpawnPositions[playerInput.playerIndex];
+        playerInput.GetComponent<PlayerController>().lookDir = playerSpawnRotations[playerInput.playerIndex];
+    }
+    #endregion
+
+    #region UNITY
+    #endregion
+}
